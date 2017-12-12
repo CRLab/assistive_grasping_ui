c616e44d6d5bf354faa734dfee9be83e63265c85
Gavi Rawson <gavirawson@gmail.com>
2017-12-11 20:48:05 -0500

    collada and stl loaders loading via bower

--- _script/config.js
+++ _script/config.js
@@ -5,7 +5,11 @@ requirejs.config({
         jquery: "../bower_components/jquery/dist/jquery",
         requirejs: "../bower_components/requirejs/require",
         mustache: "../bower_components/mustache.js/mustache",
-        three: "../bower_components/three.js/build/three"
+        three: "../bower_components/three.js/build/three",
+        eventemitter2: "../bower_components/eventemitter2/lib/eventemitter2",
+        ros3d: "../bower_components/ros3d/src/Ros3D",
+        roslib: "../bower_components/roslib/build/roslib",
+        "threex-colladaloader": "../bower_components/threex-colladaloader/ColladaLoader"
     },
     shim: {
 
